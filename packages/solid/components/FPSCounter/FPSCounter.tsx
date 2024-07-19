09c7cd0f576f2e7b7d3d2b775ee6a0bb1ad8de1c
Chris Lorenzo <chiefcll@gmail.com>
2024-07-19 14:35:34 -0600

    fix: Update FPS counter for Renderer 1.0 (#26)

--- packages/solid/components/FPSCounter/FPSCounter.tsx
+++ packages/solid/components/FPSCounter/FPSCounter.tsx
@@ -15,15 +15,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { View, Text, hexColor } from '@lightningtv/solid';
+import { View, Text } from '@lightningtv/solid';
+import type { Stage } from '@lightningjs/renderer';
 import { createSignal } from 'solid-js';
 
 const fpsStyle = {
   color: 0x000000ff,
-  height: 150,
+  height: 180,
   width: 330,
-  x: 1910,
-  y: 10,
+  x: 1900,
+  y: 6,
   mountX: 1,
   alpha: 0.8,
   zIndex: 100
@@ -31,25 +32,29 @@ const fpsStyle = {
 
 const fpsLabel = {
   x: 10,
-  fontSize: 22,
-  textColor: hexColor('#f6f6f6')
+  fontSize: 20,
+  textColor: 0xf6f6f6ff
 };
 
 const fpsValue = {
   fontSize: 22,
-  textColor: hexColor('#f6f6f6')
+  textColor: 0xf6f6f6ff
 };
 
 const [fps, setFps] = createSignal(0);
 const [avgFps, setAvgFps] = createSignal(0);
 const [minFps, setMinFps] = createSignal(99);
 const [maxFps, setMaxFps] = createSignal(0);
-const [isLoadedCount, setIsLoadedCount] = createSignal(0);
-const [isLoadingCount, setIsLoadingCount] = createSignal(0);
-const [isFailedCount, setIsFailedCount] = createSignal(0);
+const [criticalThresholdSignal, setCriticalThresholdSignal] = createSignal('');
+const [targetThresholdSignal, setTargetThresholdSignal] = createSignal('');
+const [renderableMemUsedSignal, setRenderableMemUsedSignal] = createSignal('');
+const [memUsedSignal, setMemUsedSignal] = createSignal('');
+const [renderableTexturesLoadedSignal, setRenderableTexturesLoadedSignal] = createSignal(0);
+const [loadedTexturesSignal, setLoadedTexturesSignal] = createSignal(0);
 
 let count = 0;
 let totalFps = 0;
+const infoFontSize = 14;
 
 export const resetCounter = () => {
   // clear fps
@@ -57,6 +62,10 @@ export const resetCounter = () => {
   count = 0;
   setMinFps(99);
 };
+
+function bytesToMb(bytes: number) {
+  return (bytes / 1024 / 1024).toFixed(2) + ' Mb';
+}
 
 const calcFps = (fps: number) => {
   if (!fps) return;
@@ -71,36 +80,28 @@ const calcFps = (fps: number) => {
   setAvgFps(Math.round(totalFps / count));
 };
 
-const calcPerformanceMetrics = stage => {
-  let isLoadedCount = 0;
-  let isLoadingCount = 0;
-  let isFailedCount = 0;
-  stage.txManager.textureIdCache.forEach((value: { state: string }) => {
-    if (value.state === 'loaded') {
-      isLoadedCount++;
-    }
-    if (value.state === 'loading') {
-      isLoadingCount++;
-    }
-    if (value.state === 'failed') {
-      isFailedCount++;
-    }
-  });
-  setIsLoadedCount(isLoadedCount);
-  setIsLoadingCount(isLoadingCount);
-  setIsFailedCount(isFailedCount);
-};
+function updateMemoryInfo(stage: Stage) {
+  const memInfo = stage.txMemManager.getMemoryInfo();
+  setCriticalThresholdSignal(bytesToMb(memInfo.criticalThreshold));
+  setTargetThresholdSignal(bytesToMb(memInfo.targetThreshold));
+  setRenderableMemUsedSignal(bytesToMb(memInfo.renderableMemUsed));
+  setMemUsedSignal(bytesToMb(memInfo.memUsed));
+  setRenderableTexturesLoadedSignal(memInfo.renderableTexturesLoaded);
+  setLoadedTexturesSignal(memInfo.loadedTextures);
+}
 
-// let fpsNumFrames = 0;
-// let totalFpsElapsedTime = 0;
-
+let frameCount = 0;
 export function setupFPS(root) {
   root.renderer.on('fpsUpdate', (target, fpsData) => {
     const fps = typeof fpsData === 'number' ? fpsData : fpsData.fps;
     // ignore really low fps which occur on page loads
     if (fps > 5) {
       calcFps(fps);
-      calcPerformanceMetrics(target.root.stage);
+      if (frameCount % 10 === 0) {
+        updateMemoryInfo(target.stage);
+        frameCount = 0;
+      }
+      frameCount++;
     }
   });
 }
@@ -109,53 +110,88 @@ export function setupFPS(root) {
 export const FPSCounter = props => {
   return (
     <View {...props} style={fpsStyle}>
-      <View>
+      <View y={6}>
         <Text style={fpsLabel}>FPS:</Text>
         <Text style={fpsValue} x={90}>
           {fps().toString()}
         </Text>
       </View>
 
-      <View x={160}>
+      <View y={6} x={160}>
         <Text style={fpsLabel}>AVG:</Text>
         <Text style={fpsValue} x={100}>
           {avgFps().toString()}
         </Text>
       </View>
 
-      <View x={0} y={20}>
+      <View x={0} y={26}>
         <Text style={fpsLabel}>MIN:</Text>
         <Text style={fpsValue} x={90}>
           {minFps().toString()}
         </Text>
       </View>
 
-      <View x={160} y={20}>
+      <View x={160} y={26}>
         <Text style={fpsLabel}>MAX:</Text>
         <Text style={fpsValue} x={100}>
           {maxFps().toString()}
         </Text>
       </View>
 
-      <View x={0} y={50}>
-        <Text style={fpsLabel}>Loaded Textures Cnt:</Text>
-        <Text style={fpsLabel} x={270}>
-          {isLoadedCount().toString()}
-        </Text>
-      </View>
+      <View display="flex" flexDirection="column" y={58} gap={4}>
+        <View height={infoFontSize}>
+          <Text fontSize={infoFontSize} style={fpsLabel}>
+            criticalThreshold:
+          </Text>
+          <Text fontSize={infoFontSize} style={fpsLabel} x={230}>
+            {criticalThresholdSignal()}
+          </Text>
+        </View>
 
-      <View x={0} y={80}>
-        <Text style={fpsLabel}>Loading Textures Cnt:</Text>
-        <Text style={fpsLabel} x={270}>
-          {isLoadingCount().toString()}
-        </Text>
-      </View>
+        <View height={infoFontSize}>
+          <Text fontSize={infoFontSize} style={fpsLabel}>
+            targetThreshold:
+          </Text>
+          <Text fontSize={infoFontSize} style={fpsLabel} x={230}>
+            {targetThresholdSignal()}
+          </Text>
+        </View>
 
-      <View x={0} y={110}>
-        <Text style={fpsLabel}>Failed Textures Cnt:</Text>
-        <Text style={fpsLabel} x={270}>
-          {isFailedCount().toString()}
-        </Text>
+        <View height={infoFontSize}>
+          <Text fontSize={infoFontSize} style={fpsLabel}>
+            renderableMemUsed:
+          </Text>
+          <Text fontSize={infoFontSize} style={fpsLabel} x={230}>
+            {renderableMemUsedSignal()}
+          </Text>
+        </View>
+
+        <View height={infoFontSize}>
+          <Text fontSize={infoFontSize} style={fpsLabel}>
+            memUsed:
+          </Text>
+          <Text fontSize={infoFontSize} style={fpsLabel} x={230}>
+            {memUsedSignal()}
+          </Text>
+        </View>
+
+        <View height={infoFontSize}>
+          <Text fontSize={infoFontSize} style={fpsLabel}>
+            renderableTexturesLoaded:
+          </Text>
+          <Text fontSize={infoFontSize} style={fpsLabel} x={230}>
+            {renderableTexturesLoadedSignal()}
+          </Text>
+        </View>
+
+        <View height={infoFontSize}>
+          <Text fontSize={infoFontSize} style={fpsLabel}>
+            loadedTextures:
+          </Text>
+          <Text fontSize={infoFontSize} style={fpsLabel} x={230}>
+            {loadedTexturesSignal()}
+          </Text>
+        </View>
       </View>
     </View>
   );
